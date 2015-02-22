bd77f6e4b9c0f125b203aefaa43469f12ae3bcde
kaosuxxx <kaosu22fkdls@gmail.com>
2015-02-22 15:27:25 +0900

    weather model

--- ClearWeatherApp/Weather.swift
+++ ClearWeatherApp/Weather.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class Weather: NSObject {
-   
+    
+    var aDiscription:String = ""
+    var temp_min:String = ""
+    var temp_max:String = ""
+    var main:String = ""
+    var name:String = ""
+    
+    func perseJSON(data:NSDictionary) -> Weather {
+        var weather:Weather = Weather()
+        weather.aDiscription = data.valueForKeyPath("weather.discription") as NSString
+        weather.temp_min = self.translateDouble(data.valueForKeyPath("main.temp_min") as Double)
+        weather.temp_max = self.translateDouble(data.valueForKeyPath("main.temp_max") as Double)
+        weather.main = data.valueForKeyPath("weather.main") as NSString
+        weather.name = data.valueForKey("name") as NSString
+        return weather
+    }
+    
+    func translateDouble(doubleValue:Double) -> String {
+        return NSString(format: "%1.0f゜", floor(doubleValue - 273.15))
+    }
 }
