2b328ee28849141e80fbd5b80e154ec7ecc25973
Kenichi Saito <tkcfjips@Kenichis-MacBook-Pro.local>
2015-02-22 22:57:31 +0900

    modify Weather.swift add connecting method

--- ClearWeatherApp/OpenWeatherAPIClient.swift
+++ ClearWeatherApp/OpenWeatherAPIClient.swift
@@ -9,5 +9,25 @@
 import UIKit
 
 class OpenWeatherAPIClient: NSObject {
-   
+    // シングルトン
+    class var sharedClient: OpenWeatherAPIClient {
+        struct Static {
+            static let client: OpenWeatherAPIClient = OpenWeatherAPIClient()
+        }
+        return Static.client
+    }
+    
+    func getWeather(callback: (Weather, NSError?) -> ()) {
+        let baseURL = "http://api.openweathermap.org/data/2.5/weather?"
+        let params = ["q": "Tokyo,jp"]
+        
+        Alamofire.sharedAlamofire.request(.GET, baseURL, parameters: params, encoding: .URL).responseJSON({request, response, JSON, error in
+            println(request)
+            println(response)
+            println(JSON)
+            println(error)
+            
+            callback(Weather.parseJSON(JSON), error?)
+        })
+    }
 }
--- ClearWeatherApp/Weather.swift
+++ ClearWeatherApp/Weather.swift
@@ -10,26 +10,51 @@ import UIKit
 
 class Weather: NSObject {
     
-    var aDiscription:String = ""
-    var temp_min:String = ""
-    var temp_max:String = ""
-    var main:String = ""
-    var name:String = ""
+    // Swiftには型推論と呼ばれる型を自動で判別する機能があるので型を明示しなくてもだいじょうぶ！
+    // var name: String = String() ではなく var name = String() でだいじょうぶ！
+    var aDescription = String()
+    var name = String()
+    var temp_max = String()
+    var temp_min = String()
+    var main = String()
     
-    func perseJSON(data:NSDictionary) -> Weather {
-        var weather:Weather = Weather()
-        weather.aDiscription = data.valueForKeyPath("weather.discription") as String
-        weather.temp_min = self.translateDouble(data.valueForKeyPath("main.temp_min") as Double)
-        weather.temp_max = self.translateDouble(data.valueForKeyPath("main.temp_max") as Double)
-        weather.main = data.valueForKeyPath("weather.main") as String
-        weather.name = data.valueForKey("name") as String
+    // ここはインスタンスメソッドではなくクラスメソッドで "func" の前に "class" をつける
+    // また今回は通信用のライブラリの関係で"NSDictionary"で受け取るのではなく、"AnyObject"と呼ばれる汎用型で受け取る。
+    class func parseJSON(data: AnyObject?) -> Weather {
+        let weather = Weather()
+        // jsonのパースはOptionalを考慮してこのように安全に取り出す
+        // if let文はOptionalのアンラップ構文
+        // 以前のとり方だと値が返ってこなかった時にクラッシュしてしまう
+        if let weatherData = data as? NSDictionary {
+            if let name = weatherData["name"] as? String {
+                weather.name = name
+            }
+            if let main = weatherData["main"] as? NSDictionary {
+                if let temp_max = main["temp_max"] as? Double {
+                    weather.temp_max = translateDouble(temp_max)
+
+                }
+                if let temp_min = main["temp_min"] as? Double {
+                    weather.temp_min = translateDouble(temp_min)
+                }
+            }
+            if let weatherArray = weatherData["weather"] as? NSArray {
+                if let main = weatherArray[0]["main"] as? String {
+                    weather.main = main
+                }
+                if let aDescription = weatherArray[0]["description"] as? String {
+                    weather.aDescription = aDescription
+                }
+            }
+        }
         return weather
     }
     
-    func translateDouble(doubleValue:Double) -> String {
+    class func translateDouble(doubleValue: Double) -> String {
         return NSString(format: "%1.0f゜", floor(doubleValue - 273.15))
     }
-    func translateString(StringValue:String) -> Double {
+    
+    func translateString(StringValue: String) -> Double {
         return atof(StringValue)
     }
     
