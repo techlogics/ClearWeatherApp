952b9312395a8bfcff800904a420a5669113be3e
gotenkusu48 <gotenkusu48@yahoo.co.jp>
2015-02-22 21:57:37 +0900

    DAily(ra に関数を追加した

--- ClearWeatherApp/DailyWeather.swift
+++ ClearWeatherApp/DailyWeather.swift
@@ -10,4 +10,32 @@ import UIKit
 
 class DailyWeather: NSObject {
    
+    var aDescription:String = ""
+    var temp_min:String = ""
+    var temp_max:String = ""
+    var main:String = ""
+    var dt:String = ""
+    
+    //@引数  => UnixTime:Double
+    //@返り値  => 月 + 日付の文字列:String
+    func TranslateUnixTime(UnixTimedata:Double) -> String {
+        let data = NSDate(timeIntervalSince1970: UnixTimedata)
+        return TranslateUnixTimeAtMonth(data) + TranslateUnixTimeAtDay(data)
+    }
+    
+    //UnixTimeから月を求める関数
+    func TranslateUnixTimeAtMonth(data:NSDate) ->String{
+        var outPutFormat = NSDateFormatter()
+        outPutFormat.locale = NSLocale(localeIdentifier: "ja_JP")
+        outPutFormat.dateFormat = "MM"
+        return outPutFormat.stringFromDate(data)
+    }
+    
+    //UninxTimeから日付を求める関数
+    func TranslateUnixTimeAtDay(data:NSDate) ->String{
+        var outPutFormat = NSDateFormatter()
+        outPutFormat.locale = NSLocale(localeIdentifier: "ja_JP")
+        outPutFormat.dateFormat = "dd"
+        return outPutFormat.stringFromDate(data)
+    }
 }
