c6c92c62812d22683789842fa607cb33b9bfce43
kaosuxxx <kaosu22fkdls@gmail.com>
2015-02-22 19:36:15 +0900

    kaosu commit

--- ClearWeatherApp/DailyWeather.swift
+++ ClearWeatherApp/DailyWeather.swift
@@ -9,5 +9,37 @@
 import UIKit
 
 class DailyWeather: NSObject {
-   
+    
+    var aDiscription:String = ""
+    var temp_min:String = ""
+    var temp_max:String = ""
+    var main:String = ""
+    var dt:String = ""
+    
+    func perseJSON(data:NSDictionary) -> NSArray {
+        var list = data.valueForKey("list") as NSArray
+        var weatherData:NSMutableArray!
+        
+        for lists in list {
+            var weather:DailyWeather = DailyWeather()
+            weather.aDiscription = list.valueForKeyPath("weather.discription") as String
+            weather.temp_min = self.translateDouble(list.valueForKeyPath("weather.temp_min") as Double)
+            weather.temp_max = self.translateDouble(list.valueForKeyPath("weather.temp_max") as Double)
+            weather.main = list.valueForKeyPath("weather.main") as String
+            weather.dt = self.translateTime(list.valueForKey("dt") as NSTimeInterval)
+            weatherData.addObject(weather)
+        }
+        return weatherData
+    }
+    
+    func translateTime(unixDate:NSTimeInterval) -> String {
+        var date = NSDate(timeIntervalSince1970: unixDate)
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd"
+        return formatter.stringFromDate(date) as String
+    }
+    
+    func translateDouble(doubleValue:Double) -> String {
+        return NSString(format: "%1.0f゜", floor(doubleValue - 273.15))
+    }
 }
