19eeaa8a9a1044f5a3ce4b7983217aa4758dd111
tkcfjips <tkcfjips@gmail.com>
2015-03-07 16:39:57 +0900

    modify model

--- ClearWeatherApp/OpenWeatherAPIClient.swift
+++ ClearWeatherApp/OpenWeatherAPIClient.swift
@@ -19,14 +19,15 @@ class OpenWeatherAPIClient: NSObject {
     
     func getWeather(callback: (Weather, NSError?) -> ()) {
         let baseURL = "http://api.openweathermap.org/data/2.5/weather?"
-        let params = ["q": "Tokyo,jp"]
+        let params = [
+            "q": "Tokyo,jp"
+        ]
         
         Alamofire.sharedAlamofire.request(.GET, baseURL, parameters: params, encoding: .URL).responseJSON({request, response, JSON, error in
-//            //println(request)
-//            println(response)
-//            println(JSON)
-//            println(error)
-            
+            println(request)
+            println(response)
+            println(JSON)
+            println(error)
             
             callback(Weather.parseJSON(JSON), error?)
         })
@@ -34,12 +35,18 @@ class OpenWeatherAPIClient: NSObject {
     
     func getDailyWeather(callback: ([DailyWeather], NSError?) -> ()) {
         let baseURL = "http://api.openweathermap.org/data/2.5/forecast/daily?"
-        let params = ["q" : "Tokyo", "mode" : "json", "units": "metric", "cnt": "7"]
-        
+        let params = [
+            "q"     : "Tokyo",
+            "mode"  : "json",
+            "units" : "metric",
+            "cnt"   : "7"
+        ]
         
         Alamofire.sharedAlamofire.request(.GET, baseURL, parameters: params, encoding: .URL).responseJSON({request, response, JSON, error in
-            
-             println(JSON)
+            println(request)
+            println(response)
+            println(JSON)
+            println(error)
             
             callback(DailyWeather.parseJSON(JSON), error?)
         })
