b4e769032e198873f23af82fbd96a535e5b1e785
taiyoslime <Cloudot-05@NAKAYAMATAKURO-no-MacBook-Air.local>
2015-03-17 18:46:22 +0900

    unit実装…
    NSUserDefauls の CorF というキーに保存しています
    デザインは任せました

--- ClearWeatherApp/UnitsViewController.swift
+++ ClearWeatherApp/UnitsViewController.swift
@@ -9,13 +9,77 @@
 import UIKit
 
 class UnitsViewController: UIViewController {
+    
+    
+    
+    var mySwitch : UISwitch!
+    var myLabel : UILabel!
+    let ud = NSUserDefaults.standardUserDefaults()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.view.backgroundColor = UIColor.blackColor()
+        
+        
+
+        let mySwicth: UISwitch = UISwitch()
+        mySwicth.layer.position = CGPoint(x: self.view.frame.width/2, y: 300)
+        
+        mySwicth.backgroundColor = UIColor.blackColor()
+        mySwicth.tintColor = UIColor.whiteColor()
+        
+
+        mySwicth.on = true
+        
+        mySwicth.addTarget(self, action: "onClickMySwicth:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        
+        self.view.addSubview(mySwicth)
+        
+        myLabel = UILabel(frame: CGRectMake(0,0,200,50))
+        
+        myLabel.backgroundColor = UIColor.blackColor()
+        myLabel.layer.borderColor = UIColor.greenColor().CGColor
+        myLabel.layer.borderWidth = 1.0
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = 10.0
+        myLabel.textColor = UIColor.whiteColor()
+        myLabel.font = UIFont(name: "HelveticaNeue", size: 40)
+        myLabel.textAlignment = NSTextAlignment.Center
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
+        
+
+        self.view.addSubview(myLabel)
+        
+        
+        let ud = NSUserDefaults.standardUserDefaults()
+        
+        mySwicth.on = ud.boolForKey("CorF")
+        
+        onClickMySwicth(mySwicth)
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    func onClickMySwicth(sender: UISwitch){
+        
+        if (sender.on == true)
+        {
+            myLabel.text = "°C"
+            ud.setBool(true
+                , forKey : "CtoF")
+        }
+        else
+        {
+            myLabel.text = "°F"
+            ud.setBool(false, forKey : "CtoF")
+        }
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
